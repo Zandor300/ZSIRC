eb98eb2062f685943ceedb407ee38540cc36fb4b
Zandor300 <info@zsinfo.nl>
2015-01-14 17:40:06 +0100

    Added main class.

---
+++ src/com/zandor300/zsirc/ZSIRC.java
@@ -0,0 +1,16 @@
+package com.zandor300.zsirc;
+
+/**
+ * Created by Zandor300.
+ * Date: 14-1-2015
+ * Time: 17:35
+ * PC: ZANDOR-PC
+ */
+public class ZSIRC {
+
+	public static void main(String[] args) {
+		System.out.println("Copyright: ZSinfo inc. 2015");
+		System.out.println();
+		System.out.println("Starting ZSIRC...");
+	}
+}
